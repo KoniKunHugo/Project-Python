a5a1bbc5ba32eb16d99fd30e40d84e91cd03c0eb
HugoXia <hugo.h.xia@gmail.com>
2022-11-07 19:00:15 +0100

    Commit 0.2

--- exo.py
+++ exo.py
@@ -12,4 +12,26 @@ assertionFinaleDeux = ((365 * 3) / (4 - (12 - 8)) * (rh)  > r) == (e * s  < r)
 partieDeuxUn = ((365 * 3) / (4 - (12 - 8)) * (rh)  > r) #False
 partieDeuxDeux = (e * s  < r) #False
 assertionFinaleDeux = partieDeuxUn == partieDeuxDeux #True
+
+def add(x,y):
+    return(x+y)
+
+def sub(x,y):
+    return(x-y)
+
+def multiply(x,y):
+    return(x*y)
+
+def divide(x,y):
+    return(x/y)
+
+def modulo(x,y):
+    return(x%y)
+
+def salaireNet(brut, coeff):
+    return (brut/coeff)
+
+def salaireParSec(salaireHoraire, heureParJourOuvre, nombreJoursOuvreParAn):
+    return (nombreJoursOuvreParAn*heureParJourOuvre*salaireHoraire/2592000)
+
 #FIN
